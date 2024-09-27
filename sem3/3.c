fd425f8ea7e8e924891ec8224c1221513a3a3998
Dan1aR <andan.com@mail.ru>
2024-09-27 11:40:11 +0000

    backup 2024-09-27

--- sem3/3.c
+++ sem3/3.c
@@ -4,18 +4,20 @@ int main()
 {
     unsigned long long a;
     scanf("%lld", &a);
-    printf("0x%llx\n", a);
+    size_t cnt = 0;
+    printf("0x%llx%ln\n", a, &cnt);
+
     // count number of
     // znachashix bits
-    size_t mask = 0b1111;
-    size_t cnt = 0;
-    while (a) {
-        if (a & mask) {
-            cnt++;
-        }
-        a >>= 4;
-    }
-    printf("%ld", cnt + 2);
+    // size_t mask = 0b1111;
+    // size_t cnt = 0;
+    // while (a) {
+    //     if (a & mask) {
+    //         cnt++;
+    //     }
+    //     a >>= 4;
+    // }
+    printf("%ld", cnt);
 
     return 0;
 }
