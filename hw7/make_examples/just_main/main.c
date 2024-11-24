54edbacaa50f087b4c8367d6467a2384a7b8bb73
Dan1aR <andan.com@mail.ru>
2024-11-24 10:29:33 +0000

    adding examples from lecture

---
+++ hw7/make_examples/just_main/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello from %s\n", __FUNCTION__);
+    return 0;
+}
---
+++ hw7/make_examples/templates/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "header.h"
+
+void say_hello();
+
+int main()
+{
+    say_hello();
+    return 0;
+}
